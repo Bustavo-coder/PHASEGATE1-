c4ccf6eceb8f7addb762870e7547e61354bbac48
Bustavo-coder <abdbasitegberongbe@gmail.com>
2025-08-18 15:35:34 +0100

    Tw

---
+++ JAVA/Twelvethtask.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class Twelvethtask{
+	public static void main (String [] args){
+		int [] value = {2,3,45,6,10,3,334,1101,33,3222,1};
+		Arrays.sort(value);
+		System.out.println(Arrays.toString(value));
+	
+		}
+
+
+	}
