e9ce0bcde96c0341bbb049c46c04a3ee190513a9
Bustavo-coder <abdbasitegberongbe@gmail.com>
2025-08-18 15:07:39 +0100

    eig

---
+++ JAVA/Eighttask.java
@@ -0,0 +1,12 @@
+public class  Eighttask{
+	public static void main (String [] args){
+		int [] value = {2,3,45,6,10,3,334,1101,33,3222,1};
+		int sum = 0;
+		for(int count = 0; count < value.length;count++) sum += value[count];
+		System.out.println(sum);
+	
+
+		}
+
+
+	}
