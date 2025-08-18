a98757a5dac29b6c2445175b3d4bb2c078122ca4
Bustavo-coder <abdbasitegberongbe@gmail.com>
2025-08-18 14:55:43 +0100

    d

---
+++ JAVA/fourthTask.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class fourthTask{
+	public static void main (String [] args){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter The First Numbers");
+		int userInput = input.nextInt();
+			if (userInput < 0) System.out.print("It Is negative Number");
+			else System.out.print("It Is a positive Number");
+
+		}
+
+
+	}
