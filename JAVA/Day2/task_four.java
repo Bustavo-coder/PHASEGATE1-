b609c0cd47a2590d4bb8396f9f4f6b5272e8fc20
Bustavo-coder <abdbasitegberongbe@gmail.com>
2025-08-19 16:39:28 +0100

    New

---
+++ JAVA/Day2/task_four.java
@@ -0,0 +1,11 @@
+function class task_four{
+	function static boolean getMiles(){
+		int milesToGar = 50;
+		int remaimnigLeft = 2;
+		int average = 25;
+		store = milesToGar == remaimnigLeft * average;
+		return store;
+		}
+
+
+}
