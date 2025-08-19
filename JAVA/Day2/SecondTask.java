d681d5f86867b24c066d40a0d5844b8b4ad74c69
Bustavo-coder <abdbasitegberongbe@gmail.com>
2025-08-19 13:50:45 +0100

    push

---
+++ JAVA/Day2/SecondTask.java
@@ -0,0 +1,16 @@
+public class SecondTask{
+	public static char getGrade(int score1,int score2, int score3){
+		int average = (score1 + score2 + score3) / 3;
+			char grade  = '1';
+			if (average >= 90  && average <= 100) grade = 'A';
+			else if (average >= 80  && average < 90) grade = 'B';
+			else if (average >= 70 && average < 80) grade = 'C';
+			else if (average >= 60 && average < 70) grade = 'D';
+			else if (average >= 0 && average < 60) grade = 'F';
+			return grade;
+
+		}
+
+
+
+	}
--- JAVA/Day2/TestSecondTask.java
+++ JAVA/Day2/TestSecondTask.java
@@ -1,2 +1,40 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class TestSecondTask{
+		@Test
+	public void testThatGradeReturnCorrectResult(){
+			// arrange
+		int firstScore = 90;
+		int secondScore = 80;
+		int thirdScore = 70;
+			// act
+		SecondTask calgrade = new SecondTask();
+		char result = calgrade.getGrade(firstScore,secondScore,thirdScore);
+			//assert
+		assertEquals(result,'B');
+			
+			}
+	@Test
+	public void testThatGradeReturnCorrectResultwith(){
+			// arrange
+		int firstScore = 20;
+		int secondScore = 10;
+		int thirdScore = 30;
+			// act
+		SecondTask calgrade = new SecondTask();
+		char result = calgrade.getGrade(firstScore,secondScore,thirdScore);
+			//assert
+		assertEquals(result,'F');
+			
+			}
+
+		
+	
+	}
+
+
+
 //java -jar junit-platform-console-standalone-1.9.2.jar -cp . --select-class CalculatorTest
 //javac -cp "junit-platform-console-standalone-1.9.2.jar" Calculator.java CalculatorTest.java
