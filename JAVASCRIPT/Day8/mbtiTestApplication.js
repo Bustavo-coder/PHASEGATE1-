1fe1199b41989fe69b3e7a7902ce039270860842
Bustavo-coder <abdbasitegberongbe@gmail.com>
2025-08-26 06:41:13 +0100

    fc

--- JAVASCRIPT/Day8/mbtiTestApplication.js
+++ JAVASCRIPT/Day8/mbtiTestApplication.js
@@ -34,11 +34,80 @@ const listOfQustions = [
 				a: 'A .>>> Logical Thinking , Questioning',
 				b: 'B.>>> Empathetic Feeling , and Accomodating',
 				},
+				{
+				a: 'A .>>> Organized, Orderly',
+				b: 'B.>>> Flexible, Adaptable',
+				},
+
+				{
+				a: 'A .>>> More Outgoing, Think out loud',
+				b: 'B.>>> More Reserved, Think To Yourself',
+				},
+				{
+				a: 'A .>>> Practical, Realistic, Experiential',
+				b: 'B.>>> Imaginative, Innovative, Theoretical',
+				},
+				{
+				a: 'A .>>> Candid, Straight Forward, Frank',
+				b: 'B.>>> Tactful, Kind, Encouraging',
+				},
+				{
+				a: 'A .>>> Plan, Schedule',
+				b: 'B.>>> Unplanned, Spontaneous',
+				},
+				{
+				a: 'A .>>> Seek Many Tasks, Public Activities, Interaction With Others',
+				b: 'B.>>> Seek Private, Solitary Activities With Quiet to Concentrate',
+				},
+				{
+				a: 'A .>>> Standard, Usual, Conventionals',
+				b: 'B.>>> Different, Novel, Unique',
+				},
+				{
+				a: 'A .>>> Firm, tend to Criticize, hold the line',
+				b: 'B.>>> Gentle, Tend to Appreciate, Conciliate',
+				},
+				{
+				a: 'A .>>>Regulated, Structured',
+				b: 'B.>>>Easy-Going, Live and Let Live',
+				},
+				{
+				a: 'A .>>>External, Communicative, Express Yourself',
+				b: 'B.>>>Internal, Reticent, Keep to Yourself ',
+				},
+				{
+				a: 'A .>>>Focus on here-and-now',
+				b: 'B.>>>Look to The Future, Global Perspective, Big picture ',
+				},
+				{
+				a: 'A .>>>Tough-Minded, Just',
+				b: 'B.>>>Tender-hearted, merciful',
+				},
+				{
+				a: 'A .>>>Preparation, Plan Ahead',
+				b: 'B.>>>Go With the Flow, Adapt as you Go',
+				},
+				{
+				a: 'A .>>>Active, Initiate',
+				b: 'B.>>>Reflective, Deliberate',
+				},
+				{
+				a: 'A .>>>Facts, Things, What Is',
+				b: 'B.>>>Ideas, Dreams, What Could Be',
+				},
+				{
+				a: 'A .>>>Matter of Fact, Issue-Oriented',
+				b: 'B.>>>Sensitive, People-Oriented',
+				},
+				{
+				a: 'A .>>>Control, Govern',
+				b: 'B.>>>Latitude, Freedom',
+				},
 		]
 
 function displayQuestionandStoreResponse(questions){
 storeReponse = []
-	for(let count = 0; count < questions.length;count++){
+	for(let count = 0; count < listOfQustions.length;count++){
 		for(element in questions[count]){
 			console.log(questions[count][element])
 					}
@@ -49,4 +118,18 @@ storeReponse = []
 	return storeReponse
 }
 
+arrayOfResponse = displayQuestionandStoreResponse(listOfQustions)// storing the response in an array
+
+console.log(`Hello ${userName} You selected`)
+for (let counter = 0;counter < 4;counter++){
+		optionA = 0 ; optionB = 0
+	for (let index = counter; index < listOfQustions.length; index+=4){
+		console.log(listOfQustions[index][arrayOfResponse[index]])//Printing the positionquestion and using the storesponse Array to find their response
+			if(arrayOfResponse[index] == 'a') optionA++
+			else {optionB++}
+			}
+		console.log(`Number Of A : ${optionA}`)
+		console.log(`Number Of B : ${optionB}`)
+
+	}
 
